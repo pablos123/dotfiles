980c0a7ff556b6f0611a93d69f386b205590e005
psaavedra <psaavedra@educativa.com>
2022-05-16 15:14:38 -0300

    Beginning configuration for fugitive

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,13 +1,29 @@
+-- Visuals
 vim.cmd "colorscheme gruvbox-material"
 
 vim.opt.background = "dark"
 vim.g.gruvbox_material_background = "medium" -- default value: "medium"
 
-require "plugins_conf/tree_sitter"
+require "plugins_conf/vim_airline"
 
+----------------
+
+-- Commenting
 require('Comment').setup()
 
+-- File Manager
+require "plugins_conf/nnn_nvim"
+
+-- Gir managment
+require "plugins_conf/vim_fugitive"
+
+-- Fuzzy finding
 require "plugins_conf/telescope"
+
+-- Syntax Highlighting with playground
+require "plugins_conf/tree_sitter"
+
+-- Language servers and completition setups
 
 -- the order of these two plugins is important due to
 -- doing particular setups in nvim_lsp, nvim_cmp is always
@@ -16,8 +32,4 @@ require "plugins_conf/nvim_cmp"
 require "plugins_conf/nvim_lsp"
 
 require "plugins_conf/null_ls"
-
-require "plugins_conf/vim_airline"
-
-require "plugins_conf/nnn_nvim"
 
